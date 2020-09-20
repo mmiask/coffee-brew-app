a22adb243357c5952a5eb20f0f2e0468535e6bb8
mmiask <mmiask92@gmail.com>
2020-09-20 18:05:39 +0200

    feat: added simple timer

---
+++ src/components/Timer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ms from '../../node_modules/pretty-ms'
+
+// provide time in seconds
+const brewingTime = 2;
+
+export default class Timer extends React.Component {
+    constructor(props){
+        super();
+        this.startTimer = this.startTimer.bind(this)
+        this.state = {
+            time: 0,
+            start: 0,
+            isOn: false
+        }
+    }
+
+    startTimer(e){
+        this.setState({
+            isOn: true,
+            time: this.state.time,
+            start: Date.now()
+        })
+        this.timer = setInterval(() => {
+            if(this.state.time > parseInt(brewingTime*1000)){
+                clearInterval(this.timer);
+                return;
+            }
+            this.setState({time: Date.now() - this.state.start})
+        }, 1)
+    }
+    
+    render () {
+        let start = (this.state.time === 0) ? <button onClick={this.startTimer}>start</button> : null
+        return (
+            <div>
+                <h1>Brewing time: {(this.state.time === 0) ? null : ms(this.state.time)}</h1>
+                {start}
+            </div>
+        )
+    }
+}
