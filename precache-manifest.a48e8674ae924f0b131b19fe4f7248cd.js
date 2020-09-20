afc01edc8c1740bb9b5ce58c9f12e435ffd8c73a
mmiask <mmiask92@gmail.com>
2020-09-20 18:28:48 +0200

    Updates

---
+++ precache-manifest.a48e8674ae924f0b131b19fe4f7248cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52b812b75e1c388e3fc6745de129123",
+    "url": "/coffee-brew-app/index.html"
+  },
+  {
+    "revision": "62a84bbe57100cc70fc3",
+    "url": "/coffee-brew-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "801ac01149b9c6c91c7e",
+    "url": "/coffee-brew-app/static/js/2.f8242f16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coffee-brew-app/static/js/2.f8242f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a84bbe57100cc70fc3",
+    "url": "/coffee-brew-app/static/js/main.bf42ae78.chunk.js"
+  },
+  {
+    "revision": "634cae2b33d1f00ffd06",
+    "url": "/coffee-brew-app/static/js/runtime-main.0097e01c.js"
+  }
+]);
